3c5c3b7bc940285784a94c0ebe5a20a69b5709c6
Mahmoud Magdy Hegazi <55125302+MahmoudHegazi@users.noreply.github.com>
2023-12-26 02:09:56 +0200

    Update bot_latest.js

--- bot_latest.js
+++ bot_latest.js
@@ -1,61 +1,54 @@
-let stop = false;
-let timeout = null;
-let currentScoll = 0;
-const data = [];
+const bot = {
+    limit: 0,
+    stop: false,
+    timeout: null,
+    currentScoll: 0,
+    data: [],
+    urls: [],
+    randomWait: function(maxSeconds = 12, minSeconds = 4) {
+        // in programing last num not included so add +1 to wait with option max time user said
+        maxSeconds += 1;
+        const rand = Math.floor(Math.random() * (maxSeconds - minSeconds)) + minSeconds;
+        return (rand * 1000);
+    },
 
-function scroller(cb = () => {}, timeWait = 1000, scrollSize = 500, randomMax = 12, randomMin = 4) {
-    if (stop) {
-        return false;
-    }
-    if (timeWait == 'random' || isNaN(parseInt(timeWait))) {
-        timeWait = randomWait(randomMax, randomMin);
-    }
+    start: function(timeWait = 1000, scrollSize = 500, randomMax = 12, randomMin = 4) {
+        if (bot.stop) {
+            return false;
+        }
+        if (timeWait == 'random' || isNaN(parseInt(timeWait))) {
+            timeWait = bot.randomWait(randomMax, randomMin);
+        }
 
-    let stop1 = ((window.innerHeight + window.scrollY) + 50) >= document.body.offsetHeight;
-    let stop2 = (currentScoll >= document.body.offsetHeight);
-    const cbResult = cb();
-    if (Array.isArray(cbResult) && cbResult.length > 0) {
-        data.push(...cbResult);
-    } else {
-        console.log("one of cb results is empty");
-    }
+        let stop1 = ((window.innerHeight + window.scrollY) + 50) >= document.body.offsetHeight;
+        let stop2 = (bot.currentScoll >= document.body.offsetHeight);
+        const cbResult = bot.getPageSongs();
+        if (Array.isArray(cbResult) && cbResult.length > 0) {
+            bot.data.push(...cbResult);
+        } else {
+            console.log("one of cb results is empty");
+        }
 
-    if (stop1 || stop2) {
-        console.log("completed");
-        return false;
-    }
+        if (stop1 || stop2) {
+            console.log("completed");
+            return false;
+        }
 
-    window.scrollTo(0, currentScoll);
-    currentScoll += scrollSize;
+        window.scrollTo({
+            left: 0,
+            top: bot.currentScoll,
+            behavior: 'smooth'
+        });
+        bot.currentScoll += scrollSize;
 
-    if (timeout) {
-        clearTimeout(timeout);
-    }
-    timeout = setTimeout(() => {
-        scroller(cb, timeWait);
-    }, timeWait);
-}
-
-function randomWait(maxSeconds = 12, minSeconds = 4) {
-    // in programing last num not included so add +1 to wait with option max time user said
-    maxSeconds += 1;
-    const rand = Math.floor(Math.random() * (maxSeconds - minSeconds)) + minSeconds;
-    return (rand * 1000);
-}
-
-function getPageSongs() {
-    const songs = [];
-    const urls = [];
-    // that consider scraper with performance or bot tester run on server
-    const songElms = document.querySelectorAll(".lazyLoadingList__list.sc-list-nostyle.sc-clearfix li.searchList__item");
-
-    const pageOrigin = window.location.origin;
-    if (!pageOrigin) {
-        console.error("origin of page can not recived, bot is facing un normal action, need code check");
-        return songs;
-    }
-
-    function getUrlFromBGU(backgroundImage) {
+        if (bot.timeout) {
+            clearTimeout(bot.timeout);
+        }
+        bot.timeout = setTimeout(() => {
+            bot.start(timeWait, scrollSize, randomMax, randomMin);
+        }, timeWait);
+    },
+    getUrlFromBGU: function(backgroundImage) {
         let urlParts = backgroundImage.split("(");
         let targetUrl = '';
         if (urlParts.length >= 2) {
@@ -71,54 +64,97 @@ function getPageSongs() {
             }
         }
         return targetUrl;
-    }
+    },
+    getObjIndexByUrl: function(url = '') {
+        let index = -1;
+        for (let i = 0; i < bot.data.length; i++) {
+            if (bot.data[i].url === url) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    },
+    getPageSongs: function() {
+        const songs = [];
+        const newLimit = bot.limit + 5;
+        // that consider scraper with performance or bot tester run on server
+        const songElms = Array.from(document.querySelectorAll(".lazyLoadingList__list.sc-list-nostyle.sc-clearfix li.searchList__item")).slice(0, newLimit);
+        bot.limit = newLimit;
 
-    for (let i = 0; i < songElms.length; i++) {
-        const songElm = songElms[i];
-        const song = {
-            title: '',
-            image: '',
-            url: '',
-            username: '',
-            userUrl: ''
-        };
-        let uniqueSong = true;
-        const urlElm = songElm.querySelector("div.sound__content a.soundTitle__title");
-        if (urlElm) {
-            const songUrl = urlElm.getAttribute("href");
-            if (songUrl && !urls.includes(songUrl)) {
-                const titleElm = urlElm.querySelector("span");
-                if (titleElm && titleElm.innerText) {
-                    song['title'] = titleElm.innerText;
+        const pageOrigin = window.location.origin;
+        if (!pageOrigin) {
+            console.error("origin of page can not recived, bot is facing un normal action, need code check");
+            return songs;
+        }
+
+
+
+        for (let i = 0; i < songElms.length; i++) {
+            const songElm = songElms[i];
+            const song = {
+                title: '',
+                image: '',
+                url: '',
+                username: '',
+                userUrl: ''
+            };
+            let uniqueSong = true;
+            const urlElm = songElm.querySelector("div.sound__content a.soundTitle__title");
+            if (urlElm) {
+
+                const songUrl = urlElm.getAttribute("href");
+                if (songUrl && !bot.urls.includes(songUrl)) {
+                    const titleElm = urlElm.querySelector("span");
+                    if (titleElm && titleElm.innerText) {
+                        song['title'] = titleElm.innerText;
+                    }
+                    song['url'] = pageOrigin + songUrl.trim();
+                    bot.urls.push(songUrl);
+                } else {
+                    // recap on old images
+                    const oldObjI = bot.getObjIndexByUrl(songUrl);
+                    const oldObj = (oldObjI != -1) ? bot.data[oldObjI] : null;
+                    if (!oldObj.image){
+                       bot.data.splice(oldObjI, 1);
+                    } else {
+                       uniqueSong = false;
+                    }
+                    
                 }
-                song['url'] = pageOrigin + songUrl.trim();
-                urls.push(songUrl);
-            } else {
-                uniqueSong = false;
             }
-        }
-        if (!uniqueSong) {
-            continue;
-        }
-        const userElm = songElm.querySelector("div.sound__content a.soundTitle__username");
-        if (userElm && userElm.href) {
-            song['userUrl'] = userElm.href;
-            const userName = userElm.querySelector("span.soundTitle__usernameText");
-            if (userName && userName.innerText) {
-                song['username'] = userName.innerText;
+            if (!uniqueSong) {
+                continue;
             }
-        }
-        const imgElm = songElm.querySelector("div.sound__artwork div.image.sc-artwork span[aria-role='img']");
-        if (imgElm && imgElm.style.backgroundImage) {
-            const imageUrl = getUrlFromBGU(imgElm.style.backgroundImage);
-            if (imageUrl) {
-                song['image'] = pageOrigin + imageUrl.trim();
-            } else {
-                console.log("here", imageUrl, "\n", imgElm.style.backgroundImage);
+            const userElm = songElm.querySelector("div.sound__content a.soundTitle__username");
+            if (userElm && userElm.href) {
+                song['userUrl'] = userElm.href;
+                const userName = userElm.querySelector("span.soundTitle__usernameText");
+                if (userName && userName.innerText) {
+                    song['username'] = userName.innerText;
+                }
             }
+            const imgElm = songElm.querySelector("div.sound__artwork div.image.sc-artwork span[aria-role='img']");
+            if (imgElm && imgElm.style.backgroundImage) {
+                const imageUrl = bot.getUrlFromBGU(imgElm.style.backgroundImage);
+                if (imageUrl) {
+                    song['image'] = imageUrl.trim();
+                } else {
+                    console.log("here", imageUrl, "\n", imgElm.style.backgroundImage);
+                }
+            }
+            if (song.url) {
+                songs.push(song);
+            }
+
         }
-        songs.push(song);
+        return songs;
     }
-    return songs;
 }
-scroller(getPageSongs, 'random', 500, 12, 4);
+
+
+
+console.log("bot will start after 10 seconds");
+setTimeout(() => {
+    bot.start('random', 800, 4, 1);
+}, 2000);
